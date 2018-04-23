10f8fca76d8cb3e18c2814d824af095403160c63
siriustom <linxiaoran0210@gmail.com>
2018-04-22 18:17:08 -0700

    add sign up and login page, css, and fandango logo

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET signup page. */
+router.get('/signup', function(req, res, next) {
+    res.render('account-header', { page: 'sign up' });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+    res.render('account-header', { page: 'log in' });
+});
+
 module.exports = router;
