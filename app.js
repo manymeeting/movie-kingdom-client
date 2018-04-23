59304e7de559abd7d25fa4e6a3adae83651a499c
Mokuten OH <mokuteno@gmail.com>
2018-04-22 22:55:08 -0700

    create api router to route all api requests

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var router = require('./routes/router');
+var apiRouter = require('./routes/api-router');
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', router);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/api-router.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path');
+var crypto = require('crypto');
+var router = express.Router();
+var multer = require('multer');
+
+router.get('/user/:id', function(req, res, next) {
+	//TODO: api routers implementation 
+});
+
+module.exports = router;
