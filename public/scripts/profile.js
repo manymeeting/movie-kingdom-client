820b3e66e85f0e03f2ddc6981d184cbb39a2e8ef
siriustom <linxiaoran0210@gmail.com>
2018-04-23 17:59:00 -0700

    add profile page, add edit function, and related router

---
+++ public/scripts/profile.js
@@ -0,0 +1,5 @@
+function getFirstName() {
+    console.log('lxr');
+    let firstname = document.getElementById('firstname');
+    firstname.innerHTML = 'lxr';
+}
--- routes/router.js
+++ routes/router.js
@@ -16,10 +16,41 @@ router.get('/login', function(req, res, next) {
     res.render('pg-portal', { page: 'LOGIN', title: 'Login' });
 });
 
+/* GET profile page. */
+router.get('/profile', function(req, res, next) {
+    let items = [
+        'User ID',
+        'First Name',
+        'Last Name',
+        'Address',
+        'City',
+        'State',
+        'Zip Code',
+        'Phone Number',
+        'Email'
+    ];
+    res.render('pg-portal', { page: 'PROFILE', title: items });
+});
+
+/* GET profile page. */
+router.get('/profile_edit', function(req, res, next) {
+    let items = [
+        'User ID',
+        'First Name',
+        'Last Name',
+        'Address',
+        'City',
+        'State',
+        'Zip Code',
+        'Phone Number',
+        'Email'
+    ];
+    res.render('pg-portal', { page: 'PROFILE', title: items, option: 'edit'});
+});
+
 /* GET movies page. */
 router.get('/movies', function(req, res, next) {
     res.render('pg-movie-list', { title: 'Movies' });
 });
-
 
 module.exports = router;
