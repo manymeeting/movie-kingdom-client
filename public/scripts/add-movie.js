64fbaba4ed74fd439b1fbbded5c1437c87d2234d
siriustom <linxiaoran0210@gmail.com>
2018-04-25 21:36:59 -0700

    in add movie page click add go back to halls, change time slot ui in add movie page

--- public/scripts/add-movie.js
+++ public/scripts/add-movie.js
@@ -3,5 +3,10 @@ function addNode() {
     console.log('add node');
     let node = document.createElement("INPUT");
     let root = document.getElementById("timeslot");
+    node.classList.add("form-control");
+    node.classList.add("form-control-sm");
+    node.classList.add("input-sm");
+    node.classList.add("col-sm-4");
+    node.setAttribute("type", "time");
     root.appendChild(node);
 }
