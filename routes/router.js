b9ce70a82ae430c6e2dbfe456389a430001891d3
Mokuten OH <mokuteno@gmail.com>
2018-04-25 18:00:49 -0700

    move pathdictionary to values folder

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var PathDict = require('./PathDictionary');
+var PathDict = require('../values/PathDictionary');
 
 var ctlMoviesHalls = require('../controllers/ctl-movies-halls');
 var ctlPortal = require('../controllers/ctl-portal');
