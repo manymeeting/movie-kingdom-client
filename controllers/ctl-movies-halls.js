31817c102e782968c7944e7409be822195c79158
Mokuten OH <mokuteno@gmail.com>
2018-04-25 20:51:33 -0700

    render movie list from fetched data

--- controllers/ctl-movies-halls.js
+++ controllers/ctl-movies-halls.js
@@ -2,10 +2,14 @@ var clientMessenger = require('../messengers/ClientMessenger');
 
 module.exports.movieList = function(req, res, next){
 	// get all movies
-	clientMessenger.sendGET("/movie/2", "movies")
+	var page = req.query.page ? req.query.page : 0;
+	clientMessenger.sendGET("/movies?page=" + page, "movies")
 		.then(result => {
 			console.log(result);
-			res.render('pg-movie-list', { title: 'Movies' });
+			res.render('pg-movie-list', {
+				"title": 'Movies', 
+				"movies": JSON.parse(result).content
+			});
 		})
 		.catch(err => {
 			console.log(err);
