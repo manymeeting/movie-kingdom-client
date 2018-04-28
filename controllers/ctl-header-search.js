aa501b22514973ba0cfe319f3ef1b25b6174181a
Mokuten OH <mokuteno@gmail.com>
2018-04-28 01:05:19 -0700

    add city list page

--- controllers/ctl-header-search.js
+++ controllers/ctl-header-search.js
@@ -57,16 +57,52 @@ function _fetchSchedulesByZipCode(req, res, next) {
 }
 
 function _searchCity(req, res, next) {
-	// var page = req.query.page ? req.query.page : 0;
-	// var searchValue = req.query.searchValue;
+	var page = req.query.page ? req.query.page : 0;
+	var searchValue = req.query.searchValue;
+	
+	var result = {
+		  "content": [
+		    {
+		      "cityId": 7,
+		      "cityName": "Carlsbad",
+		      "state": "CA"
+		    },
+		    {
+		      "cityId": 20,
+		      "cityName": "Lancaster",
+		      "state": "CA"
+		    },
+		    {
+		      "cityId": 53,
+		      "cityName": "Santa Monica",
+		      "state": "CA"
+		    }
+		  ],
+		  "totalPages": 1,
+		  "totalElements": 3,
+		  "last": true,
+		  "numberOfElements": 3,
+		  "sort": null,
+		  "first": true,
+		  "size": 20,
+		  "number": 0
+		};
+	res.render('pg-city-list', {
+				"title": 'Search Result', 
+				"subTitle": "Search Result For: " + searchValue,
+				"cities": result.content,
+				"searchValue": searchValue,
+				"searchType": "CITY",
+				"locationValue": searchValue
+			});
 	// clientMessenger.sendGET("/search-cities/"+ searchValue + "?page=" + page, "cities")
 	// 	.then(result => {
-	// 		console.log(result);
-	// 		res.render('pg-hall-list', {
+	// 		res.render('pg-city-list', {
 	// 			"title": 'Search Result', 
-	// 			"halls": result.content,
+	// 			"subTitle": "Search Result For: " + searchValue,
+	// 			"cities": result.content,
 	// 			"searchValue": searchValue,
-	// 			"searchType": "ZIPCODE",
+	// 			"searchType": "CITY",
 	// 			"locationValue": searchValue
 	// 		});
 	// 	})
