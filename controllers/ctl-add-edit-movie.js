b35ea0d08eb57887be18ab9f6c1cfd51efd1eecc
siriustom <linxiaoran0210@gmail.com>
2018-04-29 12:55:19 -0700

    add GET movie controller for edit movie page

--- controllers/ctl-add-edit-movie.js
+++ controllers/ctl-add-edit-movie.js
@@ -1,10 +1,22 @@
+let clientMessenger = require('../kafka/ClientMessenger');
+let API_METHOD = require('../values/constants').API_METHOD;
+let PathDict = require('../values/PathDictionary');
 
 module.exports.getAddMovie = function(req, res, next) {
-    res.render('pg-add-edit-movie', { movie_option: 'Add' });
+    res.render('pg-add-edit-movie', { movie_option: 'Add', movie: {} });
 }
 
 module.exports.getEditMovie = function(req, res, next) {
-    res.render('pg-add-edit-movie', { movie_option: 'Edit' });
+    let movieId = 20;
+    clientMessenger.send(`/movie/${movieId}`, API_METHOD.GET, "movies")
+        .then(result => {
+            res.render('pg-add-edit-movie', { movie_option: 'Edit', movie: result.movie });
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(400);
+            req.session.MKFlash.error = err;
+        });
 }
 
 module.exports.postAddMovie = function(req, res, next) {
