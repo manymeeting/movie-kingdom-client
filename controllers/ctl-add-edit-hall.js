6f63b1b624b787fc03652daf309a62200a7ff013
siriustom <linxiaoran0210@gmail.com>
2018-04-29 16:52:02 -0700

    add edit hall controller code

--- controllers/ctl-add-edit-hall.js
+++ controllers/ctl-add-edit-hall.js
@@ -8,7 +8,17 @@ module.exports.getAddHall = function(req, res, next) {
 
 module.exports.getEditHall = function(req, res, next) {
     //should get theatre info and pass it into edit hall page
-    res.render('pg-add-edit-hall', { hall_option: 'Edit', hallInfo: {} });
+    let theatreId = 503;
+    clientMessenger.send(`/theater/${theatreId}`, API_METHOD.GET, "theaters")
+        .then(result => {
+            console.log(result);
+            res.render('pg-add-edit-hall', { hall_option: 'Edit', hallInfo: result.theater });
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(400);
+            req.session.MKFlash.error = err;
+        });
 }
 
 module.exports.postAddHall = function(req, res, next) {
@@ -38,6 +48,29 @@ module.exports.postAddHall = function(req, res, next) {
 }
 
 module.exports.postEditHall = function(req, res, next) {
-    let theatreId = 503;
     //should use req.body param to do PUT operation to backend
+    let content = req.body;
+    let cityPattern = content.cityname;
+    let theatreId = content.theatreId;
+    clientMessenger.send(`/search-cities/${cityPattern}`, API_METHOD.GET, "theaters")
+        .then(result => {
+            if (result.content && result.content.length > 0) {
+                let cityId = result.content[0].cityId;
+                delete content.cityname;
+                content.cityId = cityId;
+                return clientMessenger.send(`/theater/${theatreId}`, API_METHOD.PUT, "theaters", content);
+            } else {
+                alert('cannot update this theatre due to unknown city');
+                return Promise.reject();
+            }
+        })
+        .then(result => {
+            console.log('lxr', result);
+            res.redirect(PathDict.GET.HALL_LIST);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(400);
+            req.session.MKFlash.error = err;
+        })
 }
