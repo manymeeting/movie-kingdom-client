bc32aa42c820382fcfccc9a9ad3fb37c710105bd
Mokuten OH <mokuteno@gmail.com>
2018-04-30 23:34:22 -0700

    implement filters on search schedule page

--- controllers/ctl-multi-type-search.js
+++ controllers/ctl-multi-type-search.js
@@ -1,6 +1,44 @@
 const DEFAULT_ZIPCODE = "92825"; // TODO get zipcode based on actual user location
 var clientMessenger = require('../kafka/ClientMessenger');
+var queryString = require('query-string');
 let API_METHOD = require('../values/constants').API_METHOD;
+const priceFilterDict = {
+	"0" : {
+		// all
+	},
+	"1" : {
+		minPrice: 0,
+		maxPrice: 20
+	},
+	"2" : {
+		minPrice: 21,
+		maxPrice: 50
+	},
+	"3" : {
+		minPrice: 51
+		// no max limit
+	} 
+};
+const ratingFilterDict = {
+	"0" : {
+		// all
+	},
+	"1" : {
+		minStars: 1
+	},
+	"2" : {
+		minStars: 2
+	},
+	"3" : {
+		minStars: 3
+	},
+	"4" : {
+		minStars: 4
+	},
+	"5" : {
+		minStars: 5
+	}
+};
 
 module.exports.multiTypeSearch = function(req, res, next) {
 	var searchType = req.query.searchType;
@@ -48,7 +86,18 @@ function _searchMovieByName(req, res, next) {
 function _fetchSchedulesByZipCode(req, res, next) {
 	var page = req.query.page ? req.query.page : 0;
 	var searchValue = req.query.searchValue ? req.query.searchValue : DEFAULT_ZIPCODE;
-	clientMessenger.send("/schedules-zipcdoe/"+ searchValue + "?page=" + page, API_METHOD.GET, "schedules")
+	var priceFilter = req.query.priceFilter ? req.query.priceFilter : "0";
+	var ratingFilter = req.query.ratingFilter ? req.query.ratingFilter : "0";
+
+	var queryStr = queryString.stringify({
+		page: page,
+		minPrice: priceFilterDict[priceFilter].minPrice,
+		maxPrice: priceFilterDict[priceFilter].maxPrice,
+		minStars: ratingFilterDict[ratingFilter].minStars,
+		maxStars: ratingFilterDict[ratingFilter].maxStars
+	});
+
+	clientMessenger.send("/schedules-zipcdoe/"+ searchValue + "?" + queryStr, API_METHOD.GET, "schedules")
 		.then(result => {
 			console.log(result);
 			res.render('pg-hall-list', {
@@ -56,6 +105,8 @@ function _fetchSchedulesByZipCode(req, res, next) {
 				"halls": result.content,
 				"searchValue": searchValue,
 				"searchType": "ZIPCODE",
+				"priceFilter": priceFilter,
+				"ratingFilter": ratingFilter,
 				"locationValue": searchValue
 			});
 		})
@@ -67,16 +118,29 @@ function _fetchSchedulesByZipCode(req, res, next) {
 
 function _fetchSchedulesByCityID(req, res, next){
 	var page = req.query.page ? req.query.page : 0;
-	var searchValue = req.query.searchValue ? req.query.searchValue : DEFAULT_ZIPCODE;
-	var cityName = req.query.cityName ? req.query.cityName : "";;
-	clientMessenger.send("/schedules-city/"+ searchValue + "?page=" + page, API_METHOD.GET, "schedules")
+	var searchValue = req.query.searchValue;
+	var cityName = req.query.cityName ? req.query.cityName : `CITY WITH ID ${searchValue}` ;
+	var priceFilter = req.query.priceFilter ? req.query.priceFilter : "0";
+	var ratingFilter = req.query.ratingFilter ? req.query.ratingFilter : "0";
+
+	var queryStr = queryString.stringify({
+		page: page,
+		minPrice: priceFilterDict[priceFilter].minPrice,
+		maxPrice: priceFilterDict[priceFilter].maxPrice,
+		minStars: ratingFilterDict[ratingFilter].minStars,
+		maxStars: ratingFilterDict[ratingFilter].maxStars
+	});
+
+	clientMessenger.send("/schedules-city/"+ searchValue + "?" + queryStr, API_METHOD.GET, "schedules")
 		.then(result => {
 			console.log(result);
 			res.render('pg-hall-list', {
 				"title": 'Search Result', 
 				"halls": result.content,
-				"searchValue": cityName, // show city name instead of ID
-				"searchType": "CITY", // label as city
+				"searchValue": searchValue,
+				"searchType": "CITY_ID",
+				"priceFilter": priceFilter,
+				"ratingFilter": ratingFilter,
 				"locationValue": cityName
 			});
 		})
